3071e566ef43e66d0ea031cbeca86f479f93b50c
Eric Escobar <eescobar@gruponach.com>
2022-06-03 18:10:37 -0500

    se elimino input hidden

--- resources/views/emails/form.blade.php
+++ resources/views/emails/form.blade.php
@@ -32,7 +32,6 @@
           <input type="password" name="password2" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" required>
         </div>
       </div>
-      <input type="hiden" name="correo" value="">
     <div class="md:flex md:items-center">
       <div class="md:w-1/3"></div>
       <div class="md:w-2/3">
